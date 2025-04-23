a8065a65dcd5480fffb95850c98358918007a6fa
thisfozz <kfcfozz@vk.com>
2025-04-23 20:15:26 +0500

    Редирект на страницу пользователя для залогиненных, гостям показывать home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,9 @@ class HomeController extends Controller
 {
     public function index()
     {
+        if (auth()->check()) {
+            return redirect()->route('social.show', ['id' => auth()->id()]);
+        }
         return view('home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    if (Auth::check()) {
-        return redirect()->route('social.index');
-    }
-    return view('home');
-})->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
 Route::get('/social/{id}', [App\Http\Controllers\SocialController::class, 'show'])
